b70605650ac4e4dc45e7feceaa7516f8d682b657
Warren Moore <robowaz@gmail.com>
2000-07-03 10:46:41 +0000

    Initial VAL management object

---
+++ VAL/Application/VAL.cpp
@@ -0,0 +1,25 @@
+//====---
+// VAL
+//----
+// Vapour Technology All-Purpose Library
+// Copyright 2000 Vapour Technology Ltd.
+//
+// VAL.cpp - 03/07/2000 - Warren Moore
+//	VAL management object for OS independent functions
+//
+// $Id: VAL.cpp,v 1.1 2000/07/03 10:46:41 waz Exp $
+//
+
+#include "StdAfx.h"
+
+#include "VAL.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#undef THIS_FILE
+static char THIS_FILE[] = __FILE__;
+#endif
+
+/////////
+// CVAL
+
---
+++ VAL/Application/VAL.h
@@ -0,0 +1,41 @@
+//====---
+// VAL
+//----
+// Vapour Technology All-Purpose Library
+// Copyright 2000 Vapour Technology Ltd.
+//
+// VAL.h - 03/07/2000 - Warren Moore
+//	VAL management object for OS independent functions
+//
+// $Id: VAL.h,v 1.1 2000/07/03 10:46:41 waz Exp $
+//
+
+#ifndef _VAL_
+#define _VAL_
+
+#pragma once
+
+#include "Defs.h"
+
+//#===--- Defines
+
+//#===--- Data types
+
+/////////
+// CVAL
+
+class CVAL {
+public:
+	CVAL();
+	virtual ~CVAL();
+
+//#===--- External Functions
+
+protected:
+//#===--- Internal Functions
+
+//#===--- Internal Data
+
+};
+
+#endif // _VAL_
