e0393f4e56f96b1b7a7e1f552d3b2dd7232050f7
Warren Moore <robowaz@gmail.com>
2000-07-10 10:34:38 +0000

    Initial class shell

---
+++ VAL/Application/SFX.cpp
@@ -0,0 +1,31 @@
+//====---
+// VAL
+//----
+// Vapour Technology All-Purpose Library
+// Copyright 2000 Vapour Technology Ltd.
+//
+// SFX.h - 10/07/2000 - Warren Moore
+//	Class for self-management of self-extracting wedgies
+//
+// $Id: SFX.cpp,v 1.1 2000/07/10 10:34:38 waz Exp $
+//
+
+#include "StdAfx.h"
+
+#include "VAL.h"
+#include "SFX.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#undef THIS_FILE
+static char THIS_FILE[] = __FILE__;
+#endif
+
+/////////
+// CSFX
+
+CSFX::CSFX() {
+} // Constructor
+
+CSFX::~CSFX() {
+} // Destructor
---
+++ VAL/Application/SFX.h
@@ -0,0 +1,39 @@
+//====---
+// VAL
+//----
+// Vapour Technology All-Purpose Library
+// Copyright 2000 Vapour Technology Ltd.
+//
+// SFX.h - 10/07/2000 - Warren Moore
+//	Class for self-management of self-extracting wedgies
+//
+// $Id: SFX.h,v 1.1 2000/07/10 10:34:38 waz Exp $
+//
+
+#ifndef _VAL_SFX_
+#define _VAL_SFX_
+
+#pragma once
+
+//#===--- Defines
+
+//#===--- Data types
+
+/////////
+// CSFX
+
+class CSFX {
+public:
+	CSFX();
+	~CSFX();
+
+//#===--- External Functions
+
+protected:
+//#===--- Internal Functions
+
+//#===--- Internal Data
+
+};
+
+#endif // _VAL_SFX_
