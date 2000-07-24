b87380cc9aef3dbbac6a5d9b6afdc7437f6422c0
Warren Moore <robowaz@gmail.com>
2000-07-24 21:10:59 +0000

    Working with CRCOpenGLBufferWin32 initial working version

--- VAL/Render/RenderContext.cpp
+++ VAL/Render/RenderContext.cpp
@@ -4,10 +4,10 @@
 // Vapour Technology All-Purpose Library
 // Copyright 2000 Vapour Technology Ltd.
 //
-// RenderContext.cpp - 02/07/2000 - Warren Moore
+// RenderContext.cpp - 23/07/2000 - Warren Moore
 //	Base class for render contexts
 //
-// $Id: RenderContext.cpp,v 1.1 2000/07/24 10:03:06 waz Exp $
+// $Id: RenderContext.cpp,v 1.2 2000/07/24 21:10:59 waz Exp $
 //
 
 #include "StdAfx.h"
@@ -21,24 +21,57 @@
 static char THIS_FILE[] = __FILE__;
 #endif
 
-////////////
-// CWedgie
+//#===--- Defines
+#define RC_ID								"BaseRC-23072000"
+
+///////////////////
+// CRenderContext
 
 CRenderContext::CRenderContext() {
+	m_uWidth = m_uHeight = 0;
+	m_uDepth = 0;
+	m_bCreated = false;
+	m_bEnabled = false;
 } // Contructor
 
 CRenderContext::~CRenderContext() {
 } // Destructor
 
-void CRenderContext::SetOption(int iOption, int iValue) {
-} // SetOption (Int)
+const char *CRenderContext::GetID() const {
+	return RC_ID;
+} // GetID
 
-void CRenderContext::SetOption(int iOption, const char *pcValue) {
-} // SetOption (String)
+void CRenderContext::SetOption(int iOption, unsigned int uValue) {
+	if (m_bCreated)
+		return;
+	switch (iOption) {
+		case RC_WIDTH:
+			m_uWidth = uValue;
+			break;
+		case RC_HEIGHT:
+			m_uHeight = uValue;
+			break;
+		case RC_DEPTH:
+			m_uDepth = uValue;
+			break;
+		default:
+			break;
+	}
+} // SetOption (Unsigned Int)
 
-void CRenderContext::GetOption(int iOption, int &iValue) {
-} // GetOption (Int)
-
-void CRenderContext::GetOption(int iOption, const char *&pcValue) {
-} // GetOption (String)
+void CRenderContext::GetOption(int iOption, unsigned int &uValue) {
+	switch (iOption) {
+		case RC_WIDTH:
+			uValue = m_uWidth;
+			break;
+		case RC_HEIGHT:
+			uValue = m_uHeight;
+			break;
+		case RC_DEPTH:
+			uValue = m_uDepth;
+			break;
+		default:
+			break;
+	}
+} // GetOption (Unsigned Int)
 
--- VAL/Render/RenderContext.h
+++ VAL/Render/RenderContext.h
@@ -4,10 +4,10 @@
 // Vapour Technology All-Purpose Library
 // Copyright 2000 Vapour Technology Ltd.
 //
-// RenderContext.h - 02/07/2000 - Warren Moore
+// RenderContext.h - 23/07/2000 - Warren Moore
 //	Base class for render contexts
 //
-// $Id: RenderContext.h,v 1.1 2000/07/23 19:24:18 waz Exp $
+// $Id: RenderContext.h,v 1.2 2000/07/24 21:10:59 waz Exp $
 //
 
 #ifndef _VAL_RENDERCONTEXT_
@@ -16,6 +16,8 @@
 #pragma once
 
 #include "VAL.h"
+
+#include "Image.h"
 
 //#===--- Defines
 
@@ -40,9 +42,29 @@ public:
 	virtual ~CRenderContext();
 
 //#===--- External Functions
+	//#===--- Status Functions
+	// Returns the unique ID for the context
+	virtual const char *GetID() const;
+	// Returns bool indicating whether the context has been created
+	virtual bool Active() const;
+
 	// Set a context option
-	void SetOption(int iOption, int iValue);
-	void SetOption(int iOption, const char *pcValue);
+	virtual void SetOption(int iOption, unsigned int uValue);
+	// Retrieve context options
+	virtual void GetOption(int iOption, unsigned int &uValue);
+
+	// Create the context as per the supplied options
+	virtual RCRESULT Create() = 0;
+	// Destroy the current context
+	virtual void Destroy() = 0;
+
+	// Enable the context for use
+	virtual RCRESULT Enable() = 0;
+	// Disable the context
+	virtual void Disable() = 0;
+
+	// Create an image from the render area
+	virtual RCRESULT Snapshot(CImage *&poImage) = 0;
 
 protected:
 //#===--- Internal Structures
@@ -50,7 +72,17 @@ protected:
 //#===--- Internal Functions
 
 //#===--- Internal Data
+	unsigned int m_uWidth, m_uHeight;							// Context resolution
+	unsigned int m_uDepth;												// Colour depth (in bits per pixel)
 
+	bool m_bCreated;															// Context created indicator
+	bool m_bEnabled;															// Context enabled indicator
 };
+
+//#===--- Inline Functions
+
+inline bool CRenderContext::Active() const {
+	return m_bCreated;
+} // Active
 
 #endif // _VAL_RENDERCONTEXT_
