59ba143c0cbd14a4ebbf10558140eb5785cbe72c
Warren Moore <robowaz@gmail.com>
2000-07-30 20:56:56 +0000

    Added extra function comments

--- VAL/Image/Palette.h
+++ VAL/Image/Palette.h
@@ -7,7 +7,7 @@
 // Palette.h - 19/03/2000 - Warren Moore
 //	Palette header
 //
-// $Id: Palette.h,v 1.4 2000/07/11 16:09:39 waz Exp $
+// $Id: Palette.h,v 1.5 2000/07/30 20:56:56 waz Exp $
 //
 
 #ifndef _VAL_PALETTE_
@@ -22,19 +22,19 @@
 // CPalette
 
 class CImagePalette {
-//#===--- Member Functions
 public:
+	//#===--- Constructions
 	CImagePalette(int iSize = 256, unsigned long uColour = 0);
 	CImagePalette(const CImagePalette &oCopy);
 	~CImagePalette();
 	void Copy(CImagePalette *pCopy);
 
-// Setup
+	//#===--- Settings
 	void SetSize(int iSize);
 	int GetSize() const;
 	void Clear(unsigned long uColour = 0);
 
-// Palette
+	//#===--- Entry Manipulation
 	int AddEntry(unsigned long uColour);
 	void SetEntry(int iIndex, unsigned long uColour);
 	void GetEntry(int iIndex, unsigned long &uColour) const;
