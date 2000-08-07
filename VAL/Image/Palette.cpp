9917dff75c009b1352337629e3dda8c444a484bf
Warren Moore <robowaz@gmail.com>
2000-08-07 18:58:33 +0000

    Added colour match caching

--- VAL/Image/Palette.cpp
+++ VAL/Image/Palette.cpp
@@ -7,7 +7,7 @@
 // Palette.cpp - 19/03/2000 - Warren Moore
 //	Palette implementation
 //
-// $Id: Palette.cpp,v 1.3 2000/07/11 16:09:39 waz Exp $
+// $Id: Palette.cpp,v 1.4 2000/08/07 18:58:33 waz Exp $
 //
 
 #include "stdafx.h"
@@ -23,69 +23,212 @@ static char THIS_FILE[]=__FILE__;
 //////////////////
 // CImagePalette
 
-CImagePalette::CImagePalette(int iSize, unsigned long uColour) {
-// Set the initial parameters
+CImagePalette::CImagePalette(int iSize, unsigned int uColour) {
+	// Set the initial parameters
 	m_iSize = (iSize > 256) ? 256 : (iSize < 0) ? 0 : iSize;
 	uColour &= 0x00FFFFFF;
 	m_iNextEntry = 0;
-// Clear the palette
+	// Clear the palette
 	int i = 256;
 	while (i) {
 		m_uPalette[--i] = uColour;
 	}
+	m_psCache = NULL;
 } // Constructor
 
 CImagePalette::CImagePalette(const CImagePalette &oCopy) {
-// Copy it all over
+	// Copy it all over
 	m_iSize = oCopy.m_iSize;
 	m_iNextEntry = oCopy.m_iNextEntry;
-	memcpy(m_uPalette, oCopy.m_uPalette, 256 * sizeof(unsigned long));
+	memcpy(m_uPalette, oCopy.m_uPalette, m_iSize * sizeof(unsigned int));
+	m_psCache = NULL;
 } // Copy Constructor
 
 CImagePalette::~CImagePalette() {
+	DeleteCache();
 } // Destructor
 
 void CImagePalette::Copy(CImagePalette *pCopy) {
 	ASSERT(pCopy);
-// Copy it all over
+	// Copy it all over
 	m_iSize = pCopy->m_iSize;
 	m_iNextEntry = pCopy->m_iNextEntry;
-	memcpy(m_uPalette, pCopy->m_uPalette, m_iSize * sizeof(unsigned long));
+	memcpy(m_uPalette, pCopy->m_uPalette, m_iSize * sizeof(unsigned int));
 } // Copy Constructor
 
 //#===--- Setup
 
 void CImagePalette::SetSize(int iSize) {
+	// Check the size is within limits (0 - 256)
 	m_iSize = (iSize > 256) ? 256 : (iSize < 0) ? 0 : iSize;
-	m_iNextEntry = 0;	
+	// Set the AddEntry counter
+	m_iNextEntry = 0;
+	// Delete the cache
+	DeleteCache();
 } // SetSize
 
-void CImagePalette::Clear(unsigned long uColour) {
+void CImagePalette::Clear(unsigned int uColour) {
+	// Mask the input colour
 	uColour &= 0x00FFFFFF;
+	// Loop through entries setting clear colour
 	int i = 256;
 	while (i) {
 		m_uPalette[--i] = uColour;
 	}
+	// Delete the cache
+	DeleteCache();
 } // Clear
 
 //#===--- Palette
 
-int CImagePalette::AddEntry(unsigned long uColour) {
+int CImagePalette::AddEntry(unsigned int uColour) {
 	ASSERT(m_iNextEntry < m_iSize);
+	// If not within palette size bail out
+	if (m_iNextEntry >= m_iSize)
+		return -1;
+	// Set the palette entry
 	uColour &= 0x00FFFFFF;
 	m_uPalette[m_iNextEntry] = uColour;
-	int iIndex = m_iNextEntry++;
-	return iIndex;
+	return m_iNextEntry++;
 } // AddEntry
 
-void CImagePalette::SetEntry(int iIndex, unsigned long uColour) {
+void CImagePalette::SetEntry(int iIndex, unsigned int uColour) {
 	ASSERT(iIndex < m_iSize);
+	// If not within palette size bail out
+	if (iIndex >= m_iSize) 
+		return;
+	// Set the palette entry
 	uColour &= 0x00FFFFFF;
 	m_uPalette[iIndex] = uColour;
 } // SetEntry
 
-void CImagePalette::GetEntry(int iIndex, unsigned long &uColour) const {
+void CImagePalette::GetEntry(int iIndex, unsigned int &uColour) const {
 	ASSERT(iIndex < m_iSize);
-	uColour = m_uPalette[iIndex];
+	// Get the palette entry if within palette size, 0 otherwise
+	uColour = (iIndex < m_iSize) ? m_uPalette[iIndex] : 0;
 } // GetEntry
+
+int CImagePalette::MatchColour(unsigned int uColour) {
+	// Create the cache (does nothing if already create)
+	CreateCache();
+	// Try to find the colour in the cache
+	int iVal = CheckCache(uColour);
+	if (iVal != -1)
+		return iVal;
+	// Split the colour into components
+	const int iR = (uColour >> 16) & 0x000000FF;
+	const int iG = (uColour >>  8) & 0x000000FF;
+	const int iB = (uColour      ) & 0x000000FF;
+	// Match the colour the long way
+	register int iCount = 0;
+	unsigned int uPal = 0;
+	int iLowestError = 8192;		// Huge error
+	int iRPal = 0, iGPal = 0, iBPal = 0;
+	// Loop through every colour
+	while (iCount < m_iSize) {
+		// Get the colour
+		uPal = m_uPalette[iCount];
+		// Calculate the individual component errors
+		iRPal = abs(iR - ((uPal      ) & 0x000000FF));
+		iGPal = abs(iG - ((uPal >>  8) & 0x000000FF));
+		iBPal = abs(iB - ((uPal >> 16) & 0x000000FF));
+		// Sum the errors
+		int iError = iRPal + iGPal + iBPal;
+		// Record if lowest error
+		if (iError < iLowestError) {
+			iLowestError = iError;
+			iVal = iCount;
+		}
+		// Carry on through the palette
+		iCount++;
+	}
+	// Add the cache entry
+	AddCacheEntry(uColour, iVal);
+	return iVal;
+} // MatchColour
+
+void CImagePalette::CreateCache() {
+	// Return if cache created
+	if (m_psCache)
+		return;
+	// Create the new
+	NEWBEGIN
+	m_psCache = (SCache*) new SCache[IPL_CACHE_SIZE];
+	NEWEND("CImagePalette::CreateCache - Palette cache memory")
+} // CreateCache
+
+int CImagePalette::CheckCache(unsigned int uColour) {
+	// Return invalid entry if no cache
+	if (!m_psCache)
+		return -1;
+	// Decrement all the entries
+	register int iCount = 0;
+	SCache *psEntry = m_psCache;
+	while (iCount < IPL_CACHE_SIZE) {
+		// Decrement value
+		psEntry->m_iHits--;
+		// Increment the counters
+		iCount++;
+		psEntry++;
+	}
+	// Mask the colour entry
+	uColour &= 0x00FFFFFF;
+	// Check the cache
+	iCount = 0;
+	psEntry = m_psCache;
+	while (iCount < IPL_CACHE_SIZE) {
+		// Have we found it, if so leave early
+		if (uColour == psEntry->m_uColour) {
+			psEntry->m_iHits = IPL_LRU_MAX;
+			return psEntry->m_iIndex;
+		}
+		// Increment the counters
+		iCount++;
+		psEntry++;
+	}
+	// Not found so return -1
+	return -1;
+} // CheckCache
+
+void CImagePalette::AddCacheEntry(unsigned int uColour, int iIndex) {
+	// Return if no cache
+	if (!m_psCache)
+		return;
+	// Mask the colour entry
+	uColour &= 0x00FFFFFF;
+	// Check the cache
+	register int iCount = 0;
+	SCache *psEntry = m_psCache;
+	int iLowEntry = 0;
+	int iLowHits = psEntry->m_iHits;
+	while (iCount < IPL_CACHE_SIZE) {
+		// Have we found it, set the entry and leave
+		if (psEntry->m_iHits < 0) {
+			psEntry->m_uColour = uColour;
+			psEntry->m_iIndex = iIndex;
+			psEntry->m_iHits = IPL_LRU_MAX;
+			return;
+		}
+		// If not found, find the least recently used
+		if (psEntry->m_iHits < iLowHits) {
+			iLowEntry = iCount;
+			iLowHits = psEntry->m_iHits;
+		}
+		// Increment the counters
+		iCount++;
+		psEntry++;
+	}
+	// Not found, so use the lowest found
+	psEntry = m_psCache + iLowEntry;
+	psEntry->m_uColour = uColour;
+	psEntry->m_iIndex = iIndex;
+	psEntry->m_iHits = IPL_LRU_MAX;
+} // AddCacheEntry
+
+void CImagePalette::DeleteCache() {
+	if (m_psCache) {
+		delete [] m_psCache;
+		m_psCache = NULL;
+	}
+} // DeleteCache
 
--- VAL/Image/Palette.h
+++ VAL/Image/Palette.h
@@ -7,7 +7,7 @@
 // Palette.h - 19/03/2000 - Warren Moore
 //	Palette header
 //
-// $Id: Palette.h,v 1.5 2000/07/30 20:56:56 waz Exp $
+// $Id: Palette.h,v 1.6 2000/08/07 18:58:33 waz Exp $
 //
 
 #ifndef _VAL_PALETTE_
@@ -18,36 +18,91 @@
 //#===--- Includes
 #include "VAL.h"
 
+//#===--- Defines
+#define IPL_CACHE_SIZE			50
+#define IPL_LRU_MAX					10
+
 /////////////
 // CPalette
 
 class CImagePalette {
 public:
 	//#===--- Constructions
-	CImagePalette(int iSize = 256, unsigned long uColour = 0);
+	// Create the palette of a specified size filled with a default colour (max 256)
+	CImagePalette(int iSize = 256, unsigned int uColour = 0);
 	CImagePalette(const CImagePalette &oCopy);
 	~CImagePalette();
+	// Copy the palette into current from a pointer
 	void Copy(CImagePalette *pCopy);
 
 	//#===--- Settings
+	// Set the size of colours within the palette
 	void SetSize(int iSize);
+	// Returns the max numbers of colour within the palette
 	int GetSize() const;
-	void Clear(unsigned long uColour = 0);
+	// Clears the palette with a default colour
+	void Clear(unsigned int uColour = 0);
 
 	//#===--- Entry Manipulation
-	int AddEntry(unsigned long uColour);
-	void SetEntry(int iIndex, unsigned long uColour);
-	void GetEntry(int iIndex, unsigned long &uColour) const;
+	// Add an entry to the palette (maintains an internal counter, returns the index, -1 if invalid)
+	int AddEntry(unsigned int uColour);
+	// Set a specific entry with a colour
+	void SetEntry(int iIndex, unsigned int uColour);
+	// Get a specific entry (0 if invalid index)
+	void GetEntry(int iIndex, unsigned int &uColour) const;
+
+	//#===--- Colour Matching
+	// Returns a palette index closest matching the supplied colour
+	int MatchColour(unsigned int uColour);
 
 //#===--- Internal Functions
 protected:
+	//#===--- Matched colour cache
+	// Creates a cache for recently matched values
+	void CreateCache();
+	// Returns a palette index if the colour was cached, -1 of not
+	int CheckCache(unsigned int uColour);
+	// Adds a colour index to the cache
+	void AddCacheEntry(unsigned int uColour, int iIndex);
+	// Deletes the cache
+	void DeleteCache();
+
+	//#===--- Palette entry hash table
+	// Creates a hash table of the palette entries
+
+//#===--- Internal Structures
+	// Colour matching cache
+	typedef struct SCacheStruct {
+		unsigned int m_uColour;
+		int m_iIndex;
+		int m_iHits;
+
+		SCacheStruct() {						// Inline constructor
+			m_uColour = 0xFF000000;		// Invalid colour
+			m_iIndex = -1;						// Invalid index
+			m_iHits = -1;							// Value guaranteed to be initially overwritten
+		}
+	} SCache;
+
+	// Palette hash table
+	typedef struct SHashStruct {
+		unsigned int m_uEntries;
+		int *m_piIndex;
+
+		SHashStruct() {							// Inline constructor
+			m_uEntries = 0;
+			m_piIndex = NULL;
+		}
+	} SHash;
 
 //#===--- Internal Data
 protected:
-	int m_iSize;
-	int m_iNextEntry;
+	int m_iSize;												// Max number of colour entries
+	int m_iNextEntry;										// Internal counter for AddEntry function
 
-	unsigned long m_uPalette[256];
+	unsigned int m_uPalette[256];				// Palette data
+
+	SCache *m_psCache;									// Colour match cache structure
 };
 
 //#===--- Inline Functions
