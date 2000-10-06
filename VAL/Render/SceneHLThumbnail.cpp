c0b6b100cd2e6eb29f68efe95eb838eaade98687
Warren Moore <robowaz@gmail.com>
2000-10-06 18:53:29 +0000

    Initial HalfLife thumbnail rendering code

---
+++ VAL/Render/SceneHLThumbnail.cpp
@@ -0,0 +1,32 @@
+//====---
+// VAL
+//----
+// Vapour Technology All-Purpose Library
+// Copyright 2000 Vapour Technology Ltd.
+//
+// SceneHLThumbnail.h - 06/10/2000 - Warren Moore
+//	Scene rendering class for HalfLife thumbnail production
+//
+// $Id: SceneHLThumbnail.cpp,v 1.0 2000/10/06 18:53:29 waz Exp $
+//
+
+#include "StdAfx.h"
+
+#include "VAL.h"
+#include "SceneHLThumbnail.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#undef THIS_FILE
+static char THIS_FILE[] = __FILE__;
+#endif
+
+//////////////////////
+// CSceneHLThumbnail
+
+CSceneHLThumbnail::CSceneHLThumbnail() {
+} // Contructor
+
+CSceneHLThumbnail::~CSceneHLThumbnail() {
+} // Destructor
+
---
+++ VAL/Render/SceneHLThumbnail.h
@@ -0,0 +1,46 @@
+//====---
+// VAL
+//----
+// Vapour Technology All-Purpose Library
+// Copyright 2000 Vapour Technology Ltd.
+//
+// SceneHLThumbnail.h - 06/10/2000 - Warren Moore
+//	Scene rendering class for HalfLife thumbnail production
+//
+// $Id: SceneHLThumbnail.h,v 1.0 2000/10/06 18:53:30 waz Exp $
+//
+
+#ifndef _VAL_SCENEHLTHUMBNAIL_
+#define _VAL_SCENEHLTHUMBNAIL_
+
+#pragma once
+
+#include "VAL.h"
+
+#include "Scene.h"
+
+//#===--- Defines
+
+//#===--- Data types
+
+//////////////////////
+// CSceneHLThumbnail
+
+class CScene {
+public:
+	CScene();
+	virtual ~CScene();
+
+//#===--- External Functions
+
+protected:
+//#===--- Internal Structures
+
+//#===--- Internal Functions
+
+//#===--- Internal Data
+};
+
+//#===--- Inline Functions
+
+#endif // _VAL_SCENEHLTHUMBNAIL_
