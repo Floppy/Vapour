4227370067d69bb372b5a645aa9b94ee07779783
Warren Moore <robowaz@gmail.com>
2000-10-23 19:19:33 +0000

    Option definitions removed from public export files

---
+++ VAL/AvatarFiles/AvatarFileSimsOptions.h
@@ -0,0 +1,42 @@
+//====---
+// VAL
+//----
+// Vapour Technology All-Purpose Library
+// Copyright 2000 Vapour Technology Ltd.
+//
+// AvatarFileSimsOptions.h - 23/10/2000 - Warren Moore
+//	Sims export option definitions
+//
+// $Id: AvatarFileSimsOptions.h,v 1.0 2000/10/23 19:19:33 waz Exp $
+//
+
+#pragma once
+
+#ifndef _VAL_AVATARFILESIMSOPTIONS_
+#define _VAL_AVATARFILESIMSOPTIONS_
+
+//#===--- Defines
+
+// Definitions of class options and arguments
+// Option IDs
+#define SIMS_AGE				5
+#define SIMS_SEX				6
+#define SIMS_SKINTONE			7
+#define SIMS_BUILD				8
+// Option values
+// SIMS_AGE
+#define SIMS_ADULT				1
+#define SIMS_CHILD				2
+// SIMS_SEX
+#define SIMS_MALE				1
+#define SIMS_FEMALE				2
+// SIMS_SKIN
+#define SIMS_LIGHT				1
+#define SIMS_MEDIUM				2
+#define SIMS_DARK				3
+// SIMS_BUILD
+#define SIMS_FIT				1
+#define SIMS_FAT				2
+#define SIMS_SKINNY				3
+
+#endif // _VAL_AVATARFILESIMSOPTIONS_
---
+++ VAL/AvatarFiles/AvatarFileUnrealOptions.h
@@ -0,0 +1,28 @@
+//====---
+// VAL
+//----
+// Vapour Technology All-Purpose Library
+// Copyright 2000 Vapour Technology Ltd.
+//
+// AvatarFileUnrealOptions.h - 23/10/2000 - Warren Moore
+//	Unreal export option defines
+//
+// $Id: AvatarFileUnrealOptions.h,v 1.0 2000/10/23 19:19:33 waz Exp $
+//
+
+#pragma once
+
+#ifndef _VAL_AVATARFILEUNREALOPTIONS_
+#define _VAL_AVATARFILEUNREALOPTIONS_
+
+//#===--- Defines
+
+// Definitions of class options and arguments
+// Option IDs
+#define UNREAL_SEX				5
+// Option values
+// UNREAL_SEX
+#define UNREAL_MALE				1
+#define UNREAL_FEMALE			2
+
+#endif // _VAL_AVATARFILEUNREALOPTIONS_
