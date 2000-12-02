8bb7ee452ffa2f7d2602ff4f8fc536dfe3329cb0
James Smith <james@floppy.org.uk>
2000-12-02 13:51:43 +0000

    Changed defaults in constructors

--- VAL/Model/SurfaceMaterial.cpp
+++ VAL/Model/SurfaceMaterial.cpp
@@ -7,7 +7,7 @@
 // SurfaceMaterial.cpp - 17/11/2000 - James Smith
 //	Surface material class implementation
 //
-// $Id: SurfaceMaterial.cpp,v 1.2 2000/12/02 07:34:32 warren Exp $
+// $Id: SurfaceMaterial.cpp,v 1.3 2000/12/02 13:51:43 james Exp $
 //
 
 #include "stdafx.h"
@@ -26,8 +26,9 @@ static char THIS_FILE[] = __FILE__;
 
 // Creates a surface material
 CSurfaceMaterial::CSurfaceMaterial() :
-	m_oDiffuse(),
-	m_oSpecular(),
+	m_oDiffuse(0.9F, 1.0F),
+   m_oAmbient(0.4F, 1.0F),
+	m_oSpecular(0.0F, 1.0F),
 	m_oEmissive(0.0F, 0.0F),
 	m_fShininess(0.0F),
 	m_poTexture(NULL) {
@@ -35,11 +36,12 @@ CSurfaceMaterial::CSurfaceMaterial() :
 
 // Creates a surface material with a particular texture
 CSurfaceMaterial::CSurfaceMaterial(CImage *poTexture) :
-	m_oDiffuse(),
-	m_oSpecular(),
+	m_oDiffuse(0.9F, 1.0F),
+   m_oAmbient(0.4F, 1.0F),
+	m_oSpecular(0.0F, 1.0F),
 	m_oEmissive(0.0F, 0.0F),
 	m_fShininess(0.0F),
-	m_poTexture(NULL) {
+	m_poTexture(poTexture) {
 } //CSurfaceMaterial(CImage * pTexture) 
 
 // Destructor
