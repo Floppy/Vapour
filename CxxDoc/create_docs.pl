fbd929c9d248cbd5a154a349a140f16f88e636ab
Warren Moore <robowaz@gmail.com>
2001-08-09 20:53:26 +0000

    Finished off

--- CxxDoc/create_docs.pl
+++ CxxDoc/create_docs.pl
@@ -6,8 +6,10 @@ use Env qw(VALET_SRCROOT VALET_DOCROOT);
 
 # make the eht directory
 `mkdir eht`;
+
 # move the eht files into the dir
 my $find_results = "`find $VALET_SRCROOT -name '*.eht'`";
 `cp $find_results eht`;
+
 # run CxxDoc on the source
-`CxxDoc -pn VALET -o $VALET_DOCROOT -p $VALET_SRCROOT -i $VALET_SRCROOT -eht eht -tc class -td docnode`;
+`CxxDoc -pn VALET -o $VALET_DOCROOT -p $VALET_SRCROOT -i $VALET_SRCROOT -eht eht -tc class -td docnode > /dev/null 2>&1`;
