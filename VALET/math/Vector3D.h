1b59f7f95ef4c5c507107168df7a96d74d9c04a5
Warren Moore <robowaz@gmail.com>
2001-10-02 13:35:36 +0000

    Initial (unworking) math source

---
+++ VALET/math/Vector3D.h
@@ -0,0 +1,295 @@
+//#========---
+//#=- VALET
+//#=====---
+// Vapour Technology All-Purpose Library
+// Copyright 2001 Vapour Technology Ltd.
+//
+// Add confidentiality notice
+
+#ifndef _VALET_VECTOR3D_
+#define _VALET_VECTOR3D_
+
+////////////////
+//! file 		= "VALET/math/vector3d.h"
+//! author 		= "James Smith"
+//! date 		= "01/10/2001"
+//! lib 		= libVALETmath
+//! rcsid 		= "$Id: Vector3D.h,v 1.1 2001/10/02 13:35:37 vap-warren Exp $"
+//! userlevel 	        = Normal
+//! docentry 	        = "VALET.Math.Geometry"
+
+// Class declarations
+//class CAxisRotation;
+
+//#===--- Includes
+#include "../arch/valet.h"
+
+namespace VALET {
+
+	//: 3D Vector
+	// A class representing a 3D vector - 3 components, X Y and Z.
+	//!classbug: This class hasn't even been compiled yet becuase I don't
+	//!classbug: have a Makefile yet and it's late. Don't even think about it.
+	class CVector3D {
+	
+	protected:
+	
+		//:-----
+		//: Data
+
+		double m_pComponents[3];
+		//: Vector components
+		// Stored in the order X Y Z
+
+	public:
+	
+		//:------------------------
+		//: Contruction/Destruction
+
+		CVector3D();
+		//: Default constructor.
+		// Creates a zero vector.
+	
+		CVector3D(const CVector3D & oVec);
+		//: Copy constructor.
+		//!param: oVec = vector to be copied.
+	
+		CVector3D(const double dX, const double dY, const double dZ);
+		//: 3-argument constructor.
+		//!param: dX = X component.
+		//!param: dY = Y component.
+		//!param: dZ = Z component.
+	
+		virtual ~CVector3D();
+		//: Destructor
+	
+		//:---------------------
+		//: Arithmetic operators
+	
+		CVector3D operator *(const double dScalar) const;
+		//: Multiplication by a scalar
+		// A new vector is returned, whose components are equal to
+	 	// the components of this vector multiplied by dScalar.	
+		// This is equivalent to a uniform scaling.
+		//!param: dScalar = scalar value to multiply by.
+		//!param: return = a new vector equal to this * dScalar
+	
+		CVector3D operator *(const CVector3D & oVec) const;
+		//: Multiplication by a vector
+		// A new vector is returned, whose components are equal to
+	 	// the components of this vector multiplied by the corresponding
+		// component of oVec.
+		// This is equivalent to a non-uniform scaling.
+		//!param: oVec = vector to multiply by.
+		//!param: return = a new vector equal to this * oVec
+	
+		CVector3D& operator *=(const double dScalar);
+		//: Multiplication by a scalar / Assignment
+		// All components of this vector are multiplied by dScalar.
+		// This is equivalent to a uniform scaling.
+		//!param: dScalar = scalar value to multiply by.
+		//!param: return = a reference to the vector being operated upon.
+	
+		CVector3D& operator *=(const CVector3D & oVec);
+		//: Multiplication by a vector / Assignment
+		// All components of this vector are multiplied by the corresponding
+		// component of oVec.
+		// This is equivalent to a non-uniform scaling.
+		//!param: oVec = vector to multiply by.
+		//!param: return = a reference to the vector being operated upon.
+	
+		CVector3D operator /(const double dScalar) const;
+		//: Division by a scalar
+		// A new vector is returned, whose components are equal to
+	 	// the components of this vector divided by dScalar.	
+		// This is equivalent to an inverse uniform scaling.
+		//!param: dScalar = scalar value to divide by.
+		//!param: return = a new vector equal to this / dScalar
+	
+		CVector3D operator /(const CVector3D & oVec) const;
+		//: Division by a vector
+		// A new vector is returned, whose components are equal to
+	 	// the components of this vector divided by the corresponding
+		// component of oVec.
+		// This is equivalent to an inverse non-uniform scaling.
+		//!param: oVec = vector to divide by.
+		//!param: return = a new vector equal to this / oVec
+	
+		CVector3D& operator /=(const double dScalar);
+		//: Divide by a scalar / Assignment
+		// All components of this vector are divided by dScalar.
+		// This is equivalent to an inverse uniform scaling.
+		//!param: dScalar = scalar value to divide by.
+		//!param: return = a reference to the vector being operated upon.
+	
+		CVector3D& operator /=(const CVector3D & oVec);
+		//: Division by a vector / Assignment
+		// All components of this vector are divided by the corresponding
+		// component of oVec.
+		// This is equivalent to an inverse non-uniform scaling.
+		//!param: oVec = vector to divide by.
+		//!param: return = a reference to the vector being operated upon.
+	
+		CVector3D operator +(const CVector3D & oVec) const;
+		//: Addition
+		// A new vector is returned, whose components are equal to
+	 	// the components of this vector added to the corresponding
+		// component of oVec.
+		//!param: oVec = vector to add.
+		//!param: return = a new vector equal to this + oVec
+		
+		CVector3D& operator +=(const CVector3D & oVec);
+		//: Addition / Assignment
+		// All components of this vector are added to the corresponding
+		// component of oVec.
+		//!param: oVec = vector to add.
+		//!param: return = a new vector equal to this + oVec
+	
+		CVector3D operator -(const CVector3D &oVec) const;
+		//: Subtraction
+		// A new vector is returned, whose components are equal to
+	 	// the components of this vector subtracted from the corresponding
+		// component of oVec.
+		//!param: oVec = vector to add.
+		//!param: return = a new vector equal to this + oVec
+	
+		CVector3D& operator -=(const CVector3D & oVec);
+		//: Subtraction / Assignment
+		// All components of this vector are added to the corresponding
+		// component of oVec.
+		//!param: oVec = vector to add.
+		//!param: return = a new vector equal to this + oVec
+		
+		CVector3D operator -(void) const;
+		//: Inverse operator
+		// Creates a new vector which is the inverse of this one.
+		// All components are inverted.
+		// See also Invert(), which changes the vector to it's inverse.
+		//!param: return = a new vector equal to -this.
+	
+		CVector3D& Invert();
+		//: Invert this vector.
+		// All components are inverted.
+		//!param: return = reference to this vector
+	
+		//:---------------------
+		//: Comparison operators
+	
+		bool operator ==(const CVector3D & oVec) const;
+		//: Equality
+		// Returns true if all components are identical.
+		//!param: oVec = vector to be compared.
+		//!param: return = boolean true or false.
+	
+		bool operator !=(const CVector3D & oVec) const;
+		//: Inequality
+		// Returns true if any component is different.
+		//!param: oVec = vector to be compared.
+		//!param: return = boolean true or false.
+	
+		//:--------------------
+		//: Geometric functions
+	
+		double Dot(const CVector3D & oVec) const;
+		//: Dot or Scalar product
+		// The dot product is the inverse cos of the angle 
+		// between the vectors, multiplied by their lengths.
+	 	//!param: oVec = the other vector to be used for the dot product
+	 	//!param: return = the dot product of the two vectors.
+	
+		CVector3D Cross(const CVector3D & oVec) const;
+		//: Cross or Vector product
+		// The cross product of two vectors is a vector orthogonal
+		// to both vectors (i.e. a normal). The length of the cross product
+		// represents the angle between the vectors.
+	 	//!param: oVec = the other vector to be used for the cross product
+	 	//!param: return = the cross product of the two vectors.	
+		
+		double Length() const;
+		//: Length of the vector.
+		//!param: return = double value representing the length of the vector.	
+	
+		CVector3D& SetLength(double dLength);
+		//: Set length
+		// Resizes the vector to be a certain length.
+		//!param: return = reference to this vector
+	
+		CVector3D& Normalise() const;
+		//: Normalise this vector.
+		// i.e. make the length of the vector equal to 1.
+		// Equivalent to calling SetLength(1)
+		//!param: return = reference to this vector
+	
+		//CVector3D Rotate(const CAxisRotation & oRot) const;
+		//: Rotate
+		// Returns a new vector which is this vector rotated about (0,0,0) by oRot.
+		//!param: oRot = axis-angle rotation
+		//!param: return = new vector after rotation
+	
+		//:-----------------
+		//: Access functions
+	
+		double X(void) const;
+		//: Access to the X component
+		//!param: return = the X component
+	
+		double& X(void);
+		//: Access to the X component
+		// Allows direct modfication of the component.
+		//!param: return = reference to the X component
+	
+		double Y(void) const;
+		//: Access to the Y component
+		//!param: return = the Y component
+	
+		double& Y(void);
+		//: Access to the Y component
+		// Allows direct modfication of the component.
+		//!param: return = reference to the Y component
+	
+		double Z(void) const;
+		//: Access to the Z component
+		//!param: return = the Z component
+	
+		double& Z(void);
+		//: Access to the Z component
+		// Allows direct modfication of the component.
+		//!param: return = reference to the Z component
+	
+		void ToDouble(double &dX, double &dY, double &dZ) const;
+		//: Get all three components at once.
+		//!param: dX = a double to be overwritten with the X component.
+		//!param: dY = a double to be overwritten with the Y component.
+		//!param: dZ = a double to be overwritten with the Z component.
+	
+		//:----------------
+		//: Input functions
+		
+		void FromDouble(double dX, double dY, double dZ);
+		//: Set the value of the vector
+		//!param: dX = the new X component.
+		//!param: dY = the new Y component.
+		//!param: dZ = the new Z component.
+	
+		bool ParseString(const char* strInput, int* used = NULL);
+		//: Load values from a string
+		//!todo: Document this properly - don't have time now, it's late and I can't remember
+		//!todo: what the return and parameters mean. Oops!
+	
+		//:-----------------
+		//: Output functions
+		
+		char* ToString(int iPrecision) const;
+		//: Print to a string
+		// Writes the components in the order X Y Z to a string.
+		//!param: iPrecision = maximum number of digits to print after the decimal point.
+		//!param: return = a string with the vector in it. Memory is allocated with malloc(),
+		//!param:          so you need to get rid of it using free() once you're done.
+		//!todo: Change this so that it returns a C++ string or something, so that
+		//!todo: we don't have to worry about freeing up memory that's been allocated.
+	
+	};
+	
+}
+
+#endif // _VALET_VECTOR3D_
---
+++ VALET/math/vector3d.cpp
@@ -0,0 +1,372 @@
+//#========---
+//#=- VALET
+//#=====---
+// Vapour Technology All-Purpose Library
+// Copyright 2001 Vapour Technology Ltd.
+//
+// Add confidentiality notice
+
+////////////////
+//! file 		= "VALET/math/vector3d.cpp"
+//! author 		= "James Smith"
+//! date 		= "01/10/2001"
+//! lib 		= libVALETmath
+//! rcsid 		= "$Id: vector3d.cpp,v 1.1 2001/10/02 13:35:36 vap-warren Exp $"
+//! userlevel 	        = Normal
+//! docentry 	        = "VALET.Math.Geometry"
+
+#include "vector3d.h"
+
+// VALET includes
+//#include "AxisRotation.h"
+//#include "Quaternion.h"
+
+// Standard includes
+#include <math.h>
+
+namespace VALET {
+
+	CVector3D::CVector3D() {
+	  m_pComponents[0] = 0.0F;
+	  m_pComponents[1] = 0.0F;
+	  m_pComponents[2] = 0.0F;
+	} //CVector3D()
+	
+	CVector3D::CVector3D(const CVector3D & oVec) {
+	  m_pComponents[0] = oVec.m_pComponents[0];
+	  m_pComponents[1] = oVec.m_pComponents[1];
+	  m_pComponents[2] = oVec.m_pComponents[2];
+	} //CVector3D(const CVector3D & vec)
+	
+	CVector3D::CVector3D(const double dX, const double dY, const double dZ) {
+	  m_pComponents[0] = dX;
+	  m_pComponents[1] = dY;
+	  m_pComponents[2] = dZ;
+	} //CVector3D(const double dX, const double dY, const double dZ)
+	
+	CVector3D::~CVector3D() {
+	} //~CVector3D()
+	
+	CVector3D CVector3D::operator *(const double dScalar) const {
+	  double dX = m_pComponents[0] * dScalar;
+	  double dY = m_pComponents[1] * dScalar;
+	  double dZ = m_pComponents[2] * dScalar;
+	  return CVector3D(dX,dY,dZ);
+	} //operator *(const double dScalar) const
+	
+	CVector3D CVector3D::operator *(const CVector3D & oVec) const {
+	  double dX = m_pComponents[0] * oVec.m_pComponents[0];
+	  double dY = m_pComponents[1] * oVec.m_pComponents[1];
+	  double dZ = m_pComponents[2] * oVec.m_pComponents[2];
+	  return CVector3D(dX,dY,dZ);
+	} //operator *(const CVector3D & oVec) const
+	
+	CVector3D& CVector3D::operator *=(const double dScalar) {
+	  m_pComponents[0] *= dScalar;
+	  m_pComponents[1] *= dScalar;
+	  m_pComponents[2] *= dScalar;
+	  return *this;
+	} //operator *=(const double dScalar)
+	
+	CVector3D& CVector3D::operator *=(const CVector3D & oVec) {
+	  m_pComponents[0] *= oVec.m_pComponents[0];
+	  m_pComponents[1] *= oVec.m_pComponents[1];
+	  m_pComponents[2] *= oVec.m_pComponents[2];
+	  return *this;
+	} //operator *=(const CVector3D & oVec)
+	
+	CVector3D CVector3D::operator /(const double dScalar) const {
+	  double dX = m_pComponents[0] / dScalar;
+	  double dY = m_pComponents[1] / dScalar;
+	  double dZ = m_pComponents[2] / dScalar;
+	  return CVector3D(dX,dY,dZ);	
+	} //operator /(const double dScalar) const
+	
+	CVector3D CVector3D::operator /(const CVector3D & oVec) const {
+	  double dX = m_pComponents[0] / oVec.m_pComponents[0];
+	  double dY = m_pComponents[1] / oVec.m_pComponents[1];
+	  double dZ = m_pComponents[2] / oVec.m_pComponents[2];
+	  return CVector3D(dX,dY,dZ);	
+	} //operator /(const CVector3D & oVec) const
+	
+	CVector3D& CVector3D::operator /=(const double dScalar) {
+	  m_pComponents[0] /= dScalar;
+	  m_pComponents[1] /= dScalar;
+	  m_pComponents[2] /= dScalar;
+	  return *this;
+	} //operator /=(const double dScalar)
+	
+	CVector3D& CVector3D::operator /=(const CVector3D & oVec) {
+	  m_pComponents[0] /= oVec.m_pComponents[0];
+	  m_pComponents[1] /= oVec.m_pComponents[1];
+	  m_pComponents[2] /= oVec.m_pComponents[2];
+	  return *this;	
+	} //operator /=(const CVector3D & oVec)
+	
+	CVector3D CVector3D::operator +(const CVector3D & oVec) const {
+	  double dX = m_pComponents[0] + oVec.m_pComponents[0];
+	  double dY = m_pComponents[1] + oVec.m_pComponents[1];
+	  double dZ = m_pComponents[2] + oVec.m_pComponents[2];
+	  return CVector3D(dX,dY,dZ);	
+	} //operator +(const CVector3D & vec) const
+	
+	CVector3D& CVector3D::operator +=(const CVector3D & oVec) {
+	  m_pComponents[0] += oVec.m_pComponents[0];
+	  m_pComponents[1] += oVec.m_pComponents[1];
+	  m_pComponents[2] += oVec.m_pComponents[2];
+	  return *this;
+	} //operator +=(const CVector3D & oVec)
+	
+	CVector3D CVector3D::operator -(const CVector3D & oVec) const {
+	  double dX = m_pComponents[0] - oVec.m_pComponents[0];
+	  double dY = m_pComponents[1] - oVec.m_pComponents[1];
+	  double dZ = m_pComponents[2] - oVec.m_pComponents[2];
+	  return CVector3D(dX,dY,dZ);	
+	} //operator -(const CVector3D & oVec) const
+	
+	CVector3D& CVector3D::operator -=(const CVector3D & oVec) {
+	  m_pComponents[0] -= oVec.m_pComponents[0];
+	  m_pComponents[1] -= oVec.m_pComponents[1];
+	  m_pComponents[2] -= oVec.m_pComponents[2];
+	  return *this;
+	} //operator -=(const CVector3D & oVec)
+	
+	CVector3D CVector3D::operator -(void) const {
+	  return CVector3D(-m_pComponents[0],-m_pComponents[1],-m_pComponents[2]);
+	} //operator -(void) const
+	
+	CVector3D& CVector3D::Invert() {
+	  m_pComponents[0] = -m_pComponents[0];
+	  m_pComponents[1] = -m_pComponents[1];
+	  m_pComponents[2] = -m_pComponents[2];
+	  return *this;
+	} //Invert()
+	
+	bool CVector3D::operator ==(const CVector3D & oVec) const
+	{
+	  return ((m_pComponents[0] == oVec.m_pComponents[0]) && 
+		  (m_pComponents[1] == oVec.m_pComponents[1]) && 
+		  (m_pComponents[2] == oVec.m_pComponents[2]));
+	} //operator ==(const CVector3D & oVec) const
+	
+	bool CVector3D::operator !=(const CVector3D & oVec) const
+	{
+	  return ((m_pComponents[0] != oVec.m_pComponents[0]) || 
+		  (m_pComponents[1] != oVec.m_pComponents[1]) || 
+		  (m_pComponents[2] != oVec.m_pComponents[2]));
+	} //operator !=(const CVector3D & oVec) const
+	
+	double CVector3D::Dot(const CVector3D & oVec) const {
+	  return 
+	    m_pComponents[0] * oVec.m_pComponents[0] +
+	    m_pComponents[1] * oVec.m_pComponents[1] +
+	    m_pComponents[2] * oVec.m_pComponents[2];
+	} //Dot(const CVector3D & oVec) const
+	
+	CVector3D CVector3D::Cross(const CVector3D & oVec) const {
+	  double dX = m_pComponents[1] * oVec.m_pComponents[2] - m_pComponents[2] * oVec.m_pComponents[1];
+	  double dY = m_pComponents[2] * oVec.m_pComponents[0] - m_pComponents[0] * oVec.m_pComponents[2];
+	  double dZ = m_pComponents[0] * oVec.m_pComponents[1] - m_pComponents[1] * oVec.m_pComponents[0];
+	  return CVector3D(dX,dY,dZ);
+	} //Cross(const CVector3D & oVec) const
+	
+	
+	double CVector3D::Length() const {
+	  return sqrt((m_pComponents[0] * m_pComponents[0]) + 
+		      (m_pComponents[1] * m_pComponents[1]) + 
+		      (m_pComponents[2] * m_pComponents[2]));
+	} //Length() const
+	
+	CVector3D& CVector3D::SetLength(double dLength) {  
+	  *this /= Length();
+	  *this *= dLength;
+	  return *this;
+	} //SetLength(double dLength)
+	
+	CVector3D CVector3D::Normalise() const {
+	  return *this / Length();
+	} //Normalise() const
+	
+	//CVector3D CVector3D::Rotate(const CAxisRotation & oRot) const {
+	//CQuaternion quatRotation(oRot);
+	//CQuaternion quatVector(0, *this);
+	//CQuaternion quatResult = quatRotation.Inverse() * quatVector * quatRotation;
+	//return quatResult.GetVector();
+	//} //Rotate(const CAxisRotation & oRot) const
+	
+	double CVector3D::X(void) const {
+	  return m_pComponents[0];
+	} //X(void) const
+	
+	double& CVector3D::X(void) {
+	  return m_pComponents[0];
+	} //X(void)
+	
+	double CVector3D::Y(void) const {
+	  return m_pComponents[1];
+	} //Y(void) const
+	
+	double& CVector3D::Y(void) {
+	  return m_pComponents[1];
+	} //Y(void)
+	
+	double CVector3D::Z(void) const {
+	  return m_pComponents[2];
+	} //Z(void) const
+	
+	double& CVector3D::Z(void) {
+	  return m_pComponents[2];
+	} //Z(void)
+	
+	void CVector3D::ToDouble(double& dX, double& dY, double& dZ) const {
+	  dX = m_pComponents[0];
+	  dY = m_pComponents[1];
+	  dZ = m_pComponents[2];
+	  return;
+	} //ToDouble(double& dX, double& dY, double& dZ) const
+	
+	void CVector3D::FromDouble(double dX, double dY, double dZ) {
+	  m_pComponents[0] = dX;
+	  m_pComponents[1] = dY;
+	  m_pComponents[2] = dZ;
+	  return;
+	} //FromDouble(double dX, double dY, double dZ)
+	
+	bool CVector3D::ParseString(const char* strInput, int* used) {
+	  const int iNumComponents = 3;
+	  int iCurrentComponent = 0;
+	  bool bFractionalPart = false;
+	  bool bExponentPart = false;
+	  int iStrLength = strlen(strInput);
+	  bool bRetVal = (iStrLength == 0) ? false : true;
+	  unsigned long int uliIntegerPart = 0;
+	  unsigned long int uliFractionalPart = 0;
+	  unsigned long int uliFractionalDivisor = 1;
+	  long int liExponentPart = 0;
+	  int iMantissaSign = 1;
+	  int iExponentSign = 1;
+	  int iCharIndex = 0;
+	  bool waiting = false;
+	  for (iCharIndex=0; (iCharIndex<iStrLength) && bRetVal && (iCurrentComponent < iNumComponents); iCharIndex++) {
+	    if (waiting) {
+	      // Assign to current component
+	      liExponentPart *= iExponentSign;
+	      m_pComponents[iCurrentComponent] = (iMantissaSign * (uliIntegerPart + ((double)uliFractionalPart / (double)uliFractionalDivisor))) * pow(10,liExponentPart);
+	      // Reset variables
+	      bFractionalPart = false;
+	      bExponentPart = false;
+	      uliIntegerPart = 0;
+	      uliFractionalPart = 0;
+	      uliFractionalDivisor = 1;
+	      liExponentPart = 0;
+	      iMantissaSign = 1;
+	      iExponentSign = 1;
+	      // Move on
+	      iCurrentComponent++;
+	      waiting = false;
+	    }
+	    if (isdigit(strInput[iCharIndex])) {
+	      if (bExponentPart) {
+		liExponentPart *= 10;
+		liExponentPart += strInput [iCharIndex] - '0';
+	      }
+	      else if (bFractionalPart) {
+		uliFractionalPart *= 10;
+		uliFractionalDivisor *= 10;
+		uliFractionalPart += strInput[iCharIndex] - '0';
+	      }
+	      else {
+		uliIntegerPart *= 10;
+		uliIntegerPart += strInput[iCharIndex] - '0';
+	      }
+	    }
+	    else if (strInput[iCharIndex] == '-') {
+	      if (bExponentPart) {
+		iExponentSign = -1;
+	      }
+	      else {
+		iMantissaSign = -1;
+	      }
+	    }
+	    else if (strInput[iCharIndex] == ' ') {
+	      // Waiting for next component.
+	      waiting = true;
+	    }
+	    else if (strInput[iCharIndex] == '.') {
+	      bFractionalPart = true;
+	    }
+	    else if ((strInput[iCharIndex] == 'e') || (strInput[iCharIndex] == 'E')) {
+	      bExponentPart = true;
+	    }
+	    else bRetVal = false;
+	  }
+	  // Assign last component
+	  if (bRetVal && (iCurrentComponent != iNumComponents)) {
+	    liExponentPart *= iExponentSign;
+	    m_pComponents[iCurrentComponent] = (iMantissaSign * (uliIntegerPart + ((double)uliFractionalPart / (double)uliFractionalDivisor))) * pow(10,liExponentPart);
+	  }
+	  if (iCurrentComponent != iNumComponents-1) bRetVal = false;
+	  if (used != NULL) {
+	    *used = iCharIndex;
+	  }
+	  return bRetVal;
+	} //ParseString(const char* strInput, int& used)
+	
+	char* CVector3D::ToString(int precision) const {
+	  // Check precision overrun
+	  // Maximum precision is 9 digits, becuase of overrun on unsigned long.
+	  if (precision > 9) precision = 9;
+	  // Allocate temporary buffer space.
+	  char* buffer = (char*)malloc(128);
+	  // Allocate output buffer space and initialise it to a null string.
+	  char* output = (char*)malloc(484);
+	  output[0] = 0;
+	  // Create each string and concatenate it onto the output string.
+	  for (int i=0; i<3; i++) {
+	    double num = m_pComponents[i];
+	    char sign = (num<0) ? '-' : '+';
+	    unsigned long whole_part = (int)floor(fabs(num));
+	    unsigned long fractional_part;
+	    double temp = fabs(num) - (double)whole_part;
+	    temp *= (pow(10,precision));
+	    double floor_val = floor(temp);
+	    double ceil_val = ceil(temp);
+	    if ((ceil_val - temp) <= 0.5) 
+	      fractional_part = (int) ceil_val;
+	    else
+	      fractional_part = (int) floor_val;
+	    if (precision == 0) {
+	      whole_part += fractional_part;
+	      fractional_part = 0;
+	    }
+	    int digits = 0;
+	    while ((fractional_part != 0) && (fractional_part % 10 == 0)) {
+	      fractional_part /= 10;
+	      digits++;
+	    }
+	    double temp_fractional_part = fractional_part;
+	    while (temp_fractional_part >= 1) {
+	      temp_fractional_part /= 10;
+	      digits++;
+	    }
+	    if (sign == '-') strcat(output,"-");
+	    sprintf(buffer,"%lu",whole_part);
+	    strcat(output,buffer);
+	    if (fractional_part) {
+	      strcat(output,".");
+	      for (int i=0; i<precision-digits; i++) {
+		strcat(output,"0");
+	      }
+	      sprintf(buffer,"%lu",fractional_part);
+	      strcat(output,buffer);
+	    }
+	    if (i < 2) strcat(output," ");
+	  }
+	  int length = strlen(output) + 1;
+	  output = (char*)realloc(output,length);
+	  free(buffer);
+	  return output;
+	} //ToString(int precision) const
+}
+
+
