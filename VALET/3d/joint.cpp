01abdb2fcc709b37edfd6f4759aa3c8268b80a62
James Smith <james@floppy.org.uk>
2001-10-18 19:32:45 +0000

    added rotation functions, ported from VAL::CAvatar

--- VALET/3d/joint.cpp
+++ VALET/3d/joint.cpp
@@ -11,13 +11,14 @@
 //! author 		= "James Smith"
 //! date 		= "18/10/2001"
 //! lib 		= libVALET3d
-//! rcsid 		= "$Id: joint.cpp,v 1.1 2001/10/18 15:45:16 vap-james Exp $"
+//! rcsid 		= "$Id: joint.cpp,v 1.2 2001/10/18 19:32:45 vap-james Exp $"
 //! userlevel 	        = Normal
 //! docentry 	        = "VALET.3D.Animation"
 
 #include "VALET/joint.h"
 
 // VALET includes
+#include "VALET/eulerrotation.h"
 
 // Standard includes
 #include <math.h>
@@ -38,5 +39,50 @@ namespace NValet {
    CJoint::~CJoint() {
    }
 
+   const CAxisRotation& CJoint::Rotate(const CAxisRotation& oRot, bool bLimit, bool bDamp) {
+      // Devolve to Euler rotation
+      CEulerRotation::UEulerType uType;
+      uType.m_eID = CEulerRotation::XYZs;
+      CHomTransform htTemp(oRot);
+      CEulerRotation rotEulerRotation(htTemp, uType);
+      // Damp if enabled
+      if (bDamp) rotEulerRotation *= m_vecDamping;
+      // Convert back to axis/angle
+      CAxisRotation rotDampedRotation(rotEulerRotation);
+      // Add to current rotation
+      rotDampedRotation.MergeInside(m_oRotation);
+      // Set and limit using RotateAbs with damping turned off.
+      return RotateAbs(rotDampedRotation,true,false);
+   }
+
+   const CAxisRotation& CJoint::RotateAbs(const CAxisRotation& oRot, bool bLimit, bool bDamp) {
+      if (bLimit) {
+         // Devolve to Euler rotation
+         CEulerRotation::UEulerType uType;
+         uType.m_eID = CEulerRotation::XYZs;
+         CHomTransform htTemp(oRot);
+         CEulerRotation rotEulerRotation(htTemp, uType);
+         // Damp if enabled
+         if (bDamp) rotEulerRotation *= m_vecDamping;
+         // Limit
+         rotEulerRotation.Limit(m_vecMaxRotation,m_vecMinRotation);
+         // Convert back to axis/angle
+         CAxisRotation rotLimitedRotation(rotEulerRotation);
+         // Store
+         m_oRotation = rotLimitedRotation;
+      }
+      else {
+         m_oRotation = oRot;
+      }
+      // Set dirty flags
+      m_bDirtyAngle = true;
+      m_bDirtyShape = true;
+      // Notify parents and children of dirtyness.
+      if (m_pParent != NULL) m_pParent->m_bDirtyShape = true;
+      for (int i=0; i<NumChildren(); i++) {
+         Child(i)->m_bDirtyShape = true;
+      }
+      return m_oRotation;
+   }
 
 }
--- VALET/3d/joint.h
+++ VALET/3d/joint.h
@@ -13,7 +13,7 @@
 //! author 		= "James Smith"
 //! date 		= "18/10/2001"
 //! lib 		= libVALET3d
-//! rcsid 		= "$Id: joint.h,v 1.1 2001/10/18 15:45:16 vap-james Exp $"
+//! rcsid 		= "$Id: joint.h,v 1.2 2001/10/18 19:32:45 vap-james Exp $"
 //! userlevel 	        = Normal
 //! docentry 	        = "VALET.3D.Animation"
 
@@ -28,7 +28,7 @@
 namespace NValet {
 
    //: A Joint in a skeleton
-
+   
    class CJoint {
 
    protected:
@@ -51,7 +51,7 @@ namespace NValet {
       JointPtr m_pParent;
       //: Parent joint.
 
-      vector<JointPtr> m_pChildren;
+      vector<JointPtr> m_lChildren;
       //: Child joints.
 
       vector<int> m_pVertices;
@@ -59,7 +59,10 @@ namespace NValet {
 
       vector<int> m_pFaces;
       //: A list of affected faces.
-      
+
+      CAxisRotation m_oRotation;
+      //: The current joint rotation.
+
       CHomTransform m_htTransform;
       //: The transform below this point in the skeleton.
       // This is the complete transform as far as (and including) this joint.
@@ -82,7 +85,76 @@ namespace NValet {
 
       ~CJoint();
       //: Destructor
+
+      //:-----------------
+      //: Access functions      
+
+      const CVector3D& Centre(void) const {return m_vecCentre;}
+      //: Access the joint centre.
+      //!param: return = the joint's centre of rotation.
+
+      const CVector3D& MinRotation(void) const {return m_vecMinRotation;}
+      //: Access the minimum rotation values.
+      //!param: return = the minimum X,Y and Z rotation values of the joint
+
+      CVector3D& MinRotation(void) {return m_vecMinRotation;}
+      //: Non-const access to the minimum rotation values.
+      // This can be used to change the values.
+      //!param: return = the minimum X,Y and Z rotation values of the joint
+
+      const CVector3D& MaxRotation(void) const {return m_vecMaxRotation;}
+      //: Access the maximum rotation values.
+      //!param: return = the maximum X,Y and Z rotation values of the joint
+
+      CVector3D& MaxRotation(void) {return m_vecMaxRotation;}
+      //: Non-const access to the maximum rotation values.
+      // This can be used to change the values.
+      //!param: return = the maximum X,Y and Z rotation values of the joint
+
+      const CVector3D& Damping(void) const {return m_vecDamping;}
+      //: Access the rotation damping values.
+      //!param: return = the scale factor on X, Y and Z rotations.
+
+      CVector3D& Damping(void) {return m_vecDamping;}
+      //: Non-const access to the rotation damping values.
+      // This can be used to change the values.
+      //!param: return = the scale factor on X, Y and Z rotations.
       
+      const JointPtr Parent(void) const {return m_pParent;}
+      //: Get the parent
+      //!param: return = a pointer/iterator to the parent joint.
+
+      int NumChildren(void) const {return m_lChildren.size();}
+      //: How many children does this joint have?
+      //!param: return = the number of children this joint has.
+
+      const JointPtr Child(int iChild) const {return m_lChildren[iChild];}
+      //: Access an individual child joint.
+      //!param: iChild = the index of the desired child joint - should be < NumChildren()
+      //!param: return = a pointer/iterator to the requested child.
+
+      //:--------------------
+      //: Animation functions
+
+      const CAxisRotation& Rotate(const CAxisRotation& oRot, bool bLimit = true, bool bDamp = true);
+      //: Rotate the joint relative to the current value.
+      // If joint limiting is disabled, damping is automatically disabled.
+      //!param: oRot = the new rotation - this is added to the current rotation.      
+      //!param: bClip = enable joint limits.
+      //!param: bDamp = enable rotation damping.
+      //!param: return = the new value of the rotation, after limiting and damping have been applied.
+      //!bug: There may be a rotation multiplication in the wrong order in here.
+      //!todo: Improve this function - it's not very good.
+
+      const CAxisRotation& RotateAbs(const CAxisRotation& oRot, bool bLimit = true, bool bDamp = true);
+      //: Set the absolute rotation of the joint.
+      // If joint limiting is disabled, damping is automatically disabled.
+      //!param: oRot = the new rotation - the old rotation is lost.
+      //!param: bClip = enable joint limits.
+      //!param: bDamp = enable rotation damping.
+      //!param: return = the new value of the rotation, after limiting and damping have been applied.
+      //!todo: Improve this function - it's not very good.
+            
    };
 
 }
