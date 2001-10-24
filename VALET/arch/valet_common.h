25a80b9f4521a51def85cb06e96ceef4032a8810
Warren Moore <robowaz@gmail.com>
2001-10-24 23:30:46 +0000

    Changed FILE_RESULT enum to EFileResult

--- VALET/arch/valet_common.h
+++ VALET/arch/valet_common.h
@@ -13,7 +13,7 @@
 //! file 		= "VALET/arch/valet_common.h""
 //! author 		= "Warren Moore"
 //! date 		= "23/09/2001"
-//! rcsid 		= "$Id: valet_common.h,v 1.10 2001/10/24 21:30:55 vap-james Exp $"
+//! rcsid 		= "$Id: valet_common.h,v 1.11 2001/10/24 23:30:46 vap-warren Exp $"
 //! userlevel 	= Normal
 //! docentry 	= "VALET.Arch"
 //! maindoc		= 1
@@ -27,7 +27,7 @@ namespace NVALET {
    /*#===--- Constants */
    const unsigned int m_uiStrLength = 1024;	    // Default fixed string length
 
-   enum FILE_RESULT {
+   enum EFileResult {
       FR_OK = 0,
       FR_DOES_NOT_EXIST,
       FR_FILE_ERROR,
