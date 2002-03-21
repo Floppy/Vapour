37d78c7d811a27ddc07ab245f256d9778e02e896
James Smith <james@floppy.org.uk>
2002-03-21 21:15:39 +0000

    Added SceneManager class

---
+++ StrucVis/Control/SceneManager.cpp
@@ -0,0 +1,213 @@
+//=============---
+// Arup Simulation Visualisation System
+//-------------
+// Copyright 2002 Vapour Technology Ltd.
+//
+// SceneManager.cpp
+// 19/03/2002 - James Smith
+//
+// $Id: SceneManager.cpp,v 1.1 2002/03/21 21:15:39 vap-james Exp $
+
+#include "stdafx.h"
+#include "SceneManager.h"
+
+#include "BeamElement.h"
+#include "SlabElement.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#undef THIS_FILE
+static char THIS_FILE[] = __FILE__;
+#endif
+
+///////////
+// CSceneManager
+
+typedef std::vector<CElement*>::iterator elemIter;
+
+CSceneManager::CSceneManager(CCortonaUtil *poCortona) :
+   m_poCortona(poCortona),
+   m_oViewpoint(poCortona)
+{
+}
+   
+CSceneManager::~CSceneManager() {
+   for (elemIter pElem = m_oElements.begin(); pElem != m_oElements.end(); pElem++) {
+      delete *pElem;
+   }
+}
+
+void CSceneManager::Load(void) {
+
+   // Initialise the NodeSet
+   float pfNodes[27] = {
+      -1, 0, -1,
+      -1, 0, 0,
+      -1, 0, 1,
+      0, 0, -1,
+      0, 0, 0,
+      0, 0, 1,
+      1, 0, -1,
+      1, 0, 0,      
+      1, 0, 1
+   };
+   m_oNodeSet.SetSize(9);
+   m_oNodeSet.SetDefault(pfNodes);
+
+   // Create some beams
+   CBeamElement* pBeam = new CBeamElement(m_poCortona,&m_oNodeSet);
+   pBeam->SetID(1);
+   pBeam->SetGroup(1);
+   pBeam->SetSize(0.5446f, 0.2119f, 0.0213f, 0.0128f);
+   pBeam->SetNodes(1,2);
+   pBeam->SetColourScheme(STRESS);
+   pBeam->SetStressRange(0,100);
+   pBeam->SetStresses(0,50);
+   m_oElements.push_back(pBeam);
+
+   pBeam = new CBeamElement(m_poCortona,&m_oNodeSet);
+   pBeam->SetID(2);
+   pBeam->SetGroup(1);
+   pBeam->SetSize(0.5446f, 0.2119f, 0.0213f, 0.0128f);
+   pBeam->SetNodes(2,3);
+   pBeam->SetColourScheme(STRESS);
+   pBeam->SetStressRange(0,100);
+   pBeam->SetStresses(50,100);
+   m_oElements.push_back(pBeam);
+
+   pBeam = new CBeamElement(m_poCortona,&m_oNodeSet);
+   pBeam->SetID(3);
+   pBeam->SetGroup(1);
+   pBeam->SetSize(0.5446f, 0.2119f, 0.0213f, 0.0128f);
+   pBeam->SetNodes(7,8);
+   pBeam->SetColourScheme(STRESS);
+   pBeam->SetStressRange(0,100);
+   pBeam->SetStresses(50,100);
+   m_oElements.push_back(pBeam);
+   
+   // Create a slab
+   CSlabElement* pSlab = new CSlabElement(m_poCortona,&m_oNodeSet);
+   pSlab->SetID(4);
+   pSlab->SetGroup(2);
+   pSlab->SetSize(0.3f);
+   pSlab->SetNodes(9,6,3,2,1,4,7,8,5);
+   pSlab->SetColourScheme(STRESS);
+   pSlab->SetStressRange(0,100);
+   pSlab->SetStresses(100,100,100,50,0,0,50,100,75);
+   m_oElements.push_back(pSlab);
+
+   Update();
+
+}
+
+void CSceneManager::FrameFirst(void) {
+   float pfNodes[27] = {
+      0, 0, 0,
+      0, 0, 0,
+      0, -0.5, 0,
+      0, 0, 0,
+      0, -0.25, 0,
+      0, -0.6, 0,
+      0, -0.1, 0,
+      0, -0.5, 0,      
+      0, -1, 0
+   };
+   m_oNodeSet.Displace(pfNodes);
+   // Render
+   Update();
+}
+
+void CSceneManager::FramePrev(void) {
+   float pfNodes[27] = {
+      0, 0, 0,
+      0, 0, 0,
+      0, -0.5, 0,
+      0, 0, 0,
+      0, -0.25, 0,
+      0, -0.6, 0,
+      0, -0.1, 0,
+      0, -0.5, 0,      
+      0, -1, 0
+   };
+   m_oNodeSet.Displace(pfNodes);
+   // Render
+   Update();
+}
+
+void CSceneManager::FrameNext(void) {
+   float pfNodes[27] = {
+      0, 0, 0,
+      0, 0, 0,
+      0, -0.5, 0,
+      0, 0, 0,
+      0, -0.25, 0,
+      0, -0.6, 0,
+      0, -0.1, 0,
+      0, -0.5, 0,      
+      0, -1, 0
+   };
+   m_oNodeSet.Displace(pfNodes);
+   // Render
+   Update();
+}
+
+void CSceneManager::FrameLast(void) {
+   float pfNodes[27] = {
+      0, 0, 0,
+      0, 0, 0,
+      0, -0.5, 0,
+      0, 0, 0,
+      0, -0.25, 0,
+      0, -0.6, 0,
+      0, -0.1, 0,
+      0, -0.5, 0,      
+      0, -1, 0
+   };
+   m_oNodeSet.Displace(pfNodes);
+   // Render
+   Update();
+}
+
+unsigned int CSceneManager::NumGroups(void) {
+   return m_oGroups.size();
+}
+
+unsigned int CSceneManager::NumElements(unsigned int iGroup) {
+   // If a group hasn't been set:
+   if (iGroup == 0) 
+      return m_oElements.size();
+   // Otherwise, count the elements in the group
+   int iCount = 0;
+   for (elemIter pElem = m_oElements.begin(); pElem != m_oElements.end(); pElem++) {
+      if ((*pElem)->Group() == iGroup) iCount++;
+   }   
+   return iCount;
+}
+
+TElementType CSceneManager::GroupType(unsigned int iGroup) {
+   return m_oGroups[iGroup].m_oType;
+}
+
+float CSceneManager::Temperature(unsigned int iGroup) {
+   return m_oGroups[iGroup].m_fTemperature;
+}
+
+void CSceneManager::SetVisibility(unsigned int iGroup, bool bVisible) {
+   for (elemIter pElem = m_oElements.begin(); pElem != m_oElements.end(); pElem++) {
+      if ((*pElem)->Group() == iGroup) (*pElem)->SetVisible(bVisible);
+   }   
+}
+
+void CSceneManager::SetScaleFactor(float fX, float fY, float fZ) {
+   m_oNodeSet.SetScaleFactor(fX,fY,fZ);
+}
+
+void CSceneManager::SetViewpoint(float pfPosition[3], float pfRotation[4]) {
+   //m_oViewpoint.Set(pfPosition,pfRotation);
+}
+
+void CSceneManager::Update(void) {   
+   for (elemIter pElem = m_oElements.begin(); pElem != m_oElements.end(); pElem++) {
+      (*pElem)->Display();
+   }
+}
---
+++ StrucVis/Control/SceneManager.h
@@ -0,0 +1,110 @@
+//=============---
+// Arup Simulation Visualisation System
+//-------------
+// Copyright 2002 Vapour Technology Ltd.
+//
+// SceneManager.h
+// 19/03/2002 - James Smith
+//
+// $Id: SceneManager.h,v 1.1 2002/03/21 21:15:39 vap-james Exp $
+
+#ifndef __SCENEMANAGER__
+#define __SCENEMANAGER__
+
+#include <vector>
+#include "Element.h"
+#include "Viewpoint.h"
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+
+class CSceneManager {
+//#===--- Construction/Destruction
+public:
+
+   CSceneManager(CCortonaUtil *poCortona);
+   // Constructor
+   
+   ~CSceneManager();
+   // Destructor
+
+//#===--- Member Functions
+
+   void Load(void);
+   // Load a dataset
+   // The old dataset is destroyed
+   
+   unsigned int NumFrames(void) {return m_iNumFrames;}
+   // The number of frames
+
+   void FrameFirst(void);
+   // Skips to the first frame
+
+   void FramePrev(void);
+   // Displays the previous frame
+
+   void FrameNext(void);
+   // Displays the next frame
+
+   void FrameLast(void);
+   // Skips to the last frame
+
+   unsigned int NumGroups(void);
+   // How many groups are there?
+   
+   unsigned int NumElements(unsigned int iGroup = 0);
+   // How many elements are there?
+   // If a group index is supplied, the answer is the number of elements in that group.
+
+   TElementType GroupType(unsigned int iGroup);
+   // What type is the group?
+
+   float Temperature(unsigned int iGroup);
+   // Returns the current temperature of the requested group
+
+   void SetVisibility(unsigned int iGroup, bool bVisible);
+   // Sets visibility of a group
+
+   void SetScaleFactor(float fX, float fY, float fZ);
+   // Sets displacement scale factor
+
+   void SetViewpoint(float pfPosition[3], float pfRotation[4]);
+   // Set the camera position
+
+private:
+
+   class CGroup {
+   public:
+      TElementType m_oType;
+      float m_fTemperature;
+   };
+   // Group information type
+
+   void Update(void);
+   // Updates the display of all elements
+
+//#===--- Member Variables
+protected:
+
+   CCortonaUtil *m_poCortona;
+   // Cortona Utility class
+
+   CNodeSet m_oNodeSet;
+   // The set of nodes
+   
+   std::vector<CElement*> m_oElements;
+   // Element list
+
+   std::vector<CGroup> m_oGroups;
+   // Group information
+
+   CViewpoint m_oViewpoint;
+   // The virtual camera
+
+   unsigned int m_iNumFrames;
+   // The number of frames in the sequence
+
+};
+
+#endif // __SCENEMANAGER__
